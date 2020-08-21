e5a34fee090db4f258a8e7d38aade0011e3c476f
Matilda <matilda.dahlstrom@gmail.com>
2020-08-21 08:53:09 +0200

    Added shiny new button (#3)
    * Added shiny new button
    
    * Ran Prettier to fix quotation mark issue

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+	function roomAlert() {
+		alert('In the future this button will create a new room. \nBut not today.');
+	}
+
 	return (
 		<div className="App">
 			<header className="App-header">
@@ -10,6 +14,7 @@ function App() {
 				<p>
 					Edit <code>src/App.tsx</code> and save to reload.
 				</p>
+				<button onClick={roomAlert}>Create new room</button>
 				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
 					Learn React
 				</a>
